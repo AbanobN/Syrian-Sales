fcb2a02f9d8c71a501c5cd2b16e32182fe328647
AbanobN <abanob.nabil.nagib@gmail.com>
2024-12-15 13:49:15 +0200

    Make Project Structure

--- app/src/main/java/com/example/syrian_sales/view/main/MainActivity.java
+++ app/src/main/java/com/example/syrian_sales/view/main/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.syrian_sales;
+package com.example.syrian_sales.view.main;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.syrian_sales.R;
 
 public class MainActivity extends AppCompatActivity {
 
